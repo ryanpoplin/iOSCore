ca24565bcbc21b31f456e69b2c733018361d0959
Byrdann Fox <mobileupstate@gmail.com>
2015-02-13 01:41:46 -0500

    init...

---
+++ iOSCore/iOSCore/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOSCore
+//
+//  Created by Byrdann Fox on 2/13/15.
+//  Copyright (c) 2015 ExcepApps, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
